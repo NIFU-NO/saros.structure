2a30c962c6ec0ed054e854d757b61c20722cc859
sda030 <stephus.daus@gmail.com>
2024-04-06 16:17:51 +0200

    -feat: `copy_folder_contents_to_dir()` now has argument `only_copy_folders`

--- R/copy_folder_contents_to_dir.R
+++ R/copy_folder_contents_to_dir.R
@@ -2,6 +2,7 @@
 #'
 #' @param to,from String, path from where to copy the contents, and where to copy them to.
 #' @param overwrite Flag. Defaults to FALSE.
+#' @param only_copy_folders Flag. Defaults to FALSE. If TRUE, only copies folders.
 #'
 #' @return NULL
 #' @export
@@ -12,11 +13,18 @@
 copy_folder_contents_to_dir <-
   function(from,
            to,
-           overwrite = FALSE) {
+           overwrite = FALSE,
+           only_copy_folders = FALSE) {
 
-    subfolders <- list.dirs(from, full.names = FALSE, recursive = FALSE)
+    if(isFALSE(only_copy_folders)) {
+    subfolders <- list.dirs(path = from, full.names = FALSE, recursive = FALSE)
     lapply(subfolders, function(x) fs::dir_copy(path = fs::path_dir(fs::path(from, x)),
                                                 new_path = fs::path(to),
                                                 overwrite = overwrite))
+    } else {
+      subfolders <- list.dirs(path = from, full.names = FALSE, recursive = TRUE)
+      lapply(subfolders, function(x) dir.create(path = fs::path_dir(fs::path(to, x)),
+                                                recursive = TRUE, showWarnings = FALSE))
+    }
     cli::cli_inform("Copied: {subfolders}")
   }
